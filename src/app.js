7701a0156d96f9e53fe03ffb9b08edc3042129ad
jhuequeman <huequeman@gmail.com>
2025-05-26 00:02:04 +0000

    generadorExcusas_jhuequeman

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+ document.getElementById("excuse").innerHTML= generador();
+
+    
+
 };
+
+function generador(){
+
+
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let whoIndex = Math.floor( Math.random() * who.length);
+let actionIndex = Math.floor(Math.random() * what.length);
+let whatIndex = Math.floor(Math.random() * what.length);
+let whenIndex = Math.floor(Math.random() * when.length);
+
+
+return who[whoIndex]+" " + action[actionIndex]+" " + what[whatIndex] +" " + when[whenIndex];
+
+}
